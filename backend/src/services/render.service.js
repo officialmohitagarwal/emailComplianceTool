dd64d97ffb3330112053c4a44907b444a187a3cf
Mohit Agarwal <mohitagarwal@Mohits-MacBook-Air-M4.local>
2026-09-08 00:21:32 +0530

    Fix for backend pupeeter 4

--- backend/src/services/render.service.js
+++ backend/src/services/render.service.js
@@ -3,15 +3,7 @@ import puppeteer from "puppeteer";
 export const renderEmail = async ({ html }) => {
 
     const browser = await puppeteer.launch({
-        headless: true,
-
-        executablePath: "/usr/bin/google-chrome-stable",
-
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage"
-        ]
+        headless: true
     });
 
     const page = await browser.newPage();
